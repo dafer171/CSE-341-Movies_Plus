1d4f0692bae1a9faf69952ba2240a693cad81c68
David <davidp@ntestinc.com>
2022-10-05 21:41:22 +0200

    auth0 render

--- server.js
+++ server.js
@@ -3,11 +3,31 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
 const port = process.env.PORT || 8080;
 const app = express();
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 app
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
