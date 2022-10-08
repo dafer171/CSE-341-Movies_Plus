998e8dca5f9dba5d445df9418a904bc32b7a5967
David <davidp@ntestinc.com>
2022-10-08 23:00:37 +0200

    index users

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/movies', require('./movies'));
+router.use('/users', require('./users'));
 /*
 routes.get('/', (req,res) => {
     res.send('David Peña');
