d540095104265c6969ef9a74f8c514185ca0c015
David <davidp@ntestinc.com>
2022-10-08 22:49:59 +0200

    user collection routes controller

---
+++ controllers/users.js
@@ -0,0 +1,109 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = (req, res) => {
+  mongodb
+    .getDb()
+    .db()
+    .collection('users')
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+};
+
+const getSingle = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid user id to find a user.');
+  }
+  const userId = new ObjectId(req.params.id);
+  mongodb
+    .getDb()
+    .db()
+    .collection('users')
+    .find({ _id: userId })
+    .toArray((err, result) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(result[0]);
+    });
+};
+
+const createUser = async (req, res) => {
+  try {
+    if (!req.body.nickname || !req.body.userName || !req.body.userLastName || !req.body.password) {
+      res.status(400).send({ message: 'Content can not be empty!' });
+      return;
+    }
+
+    const user = {
+      nickname: req.body.nickname,
+      userName: req.body.userName,
+      userLastName: req.body.userLastName,
+      password: req.body.password
+    };
+    const response = await mongodb.getDb().db().collection('users').insertOne(user);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while creating the user.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    // be aware of updateOne if you only want to update specific fields
+    const user = {
+      nickname: req.body.nickname,
+      userName: req.body.userName,
+      userLastName: req.body.userLastName,
+      password: req.body.password
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .replaceOne({ _id: usersId }, movie);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const usersId = new ObjectId(req.params.id);
+    const response = await mongodb.getDb().db().collection('users').remove({ _id: usersId }, true);
+    console.log(response);
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Some error occurred while deleting the user.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  getAll,
+  getSingle,
+  createUser,
+  updateUser,
+  deleteUser
+};
